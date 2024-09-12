ffc379d65f081723c77e34490679886c3bf690c1
Nese <neseilhannn@gmail.com>
2024-09-12 16:17:02 +0300

    versioning and fixes.

--- product-service/src/main/java/dev/example/model/Product.java
+++ product-service/src/main/java/dev/example/model/Product.java
@@ -1,5 +1,6 @@
 package dev.example.model;
 
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,5 +16,36 @@ public class Product {
     private String description;
     private Double price;
 
-    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
 }
